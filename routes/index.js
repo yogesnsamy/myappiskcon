76a00d65eacd5c5b3c0c95ef36aa7fe9b89dcbdc
yogeswari narayasamy <yogesn@my.ibm.com>
2019-01-30 18:46:07 +0800

    working album with a lightbox gallery

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+//const google = require('googleapis');
+const { google } = require('googleapis');
+const fs = require('fs');
+const key = require('./config.json');
+const drive = google.drive('v3');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let images;
+  const jwtClient = new google.auth.JWT(
+    key.client_email,
+    null,
+    key.private_key,
+    ['https://www.googleapis.com/auth/drive'],
+    null
+  );
+  //------------------------
+  jwtClient.authorize(authErr => {
+    if (authErr) {
+      console.log(authErr);
+      return;
+    }
+    // Make an authorized requests
+
+    // List Drive files.
+    drive.files.list(
+      {
+        auth: jwtClient
+      },
+      (listErr, resp) => {
+        if (listErr) {
+          console.log(listErr);
+          return;
+        }
+        images = resp.data.files.filter(file => file.mimeType == 'image/jpeg');
+        // console.log(images);
+        //+++++++++++++++++++++++
+        res.render('index', {
+          title: 'cicak',
+          images: images
+        });
+        //++++++++++++++++++++++++
+      }
+    );
+  });
+  //------------------------
+});
+
+module.exports = router;
